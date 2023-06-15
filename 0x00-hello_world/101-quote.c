5651f84ae477bba97a36838fcf6f17b9273eb0f6
SalvatorePaul <kehindepaul105@gmail.com>
2023-06-15 16:10:08 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include<studio.h>
+#include<unitd.h>
+
+/**
+ * main - prints "Other functions
+ * apart from printf and puts"
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
