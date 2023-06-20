49d736b82061c453f5321f35d0b8ff5033bb8a18
SalvatorePaul <kehindepaul105@gmail.com>
2023-06-20 16:03:30 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * int _abs - a function that computes the absolute value of an integer
+ * @c: the integer to check
+ * Return: the anolute value of an integer
+ */
+
+int _abs(int abs)
+{
+	 if ( abs >= 0)
+	return (abs);
+	 else
+	return (-abs);
+}
