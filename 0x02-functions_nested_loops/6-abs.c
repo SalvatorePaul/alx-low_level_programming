726b732a5065177045fdb7b47003a1f61ba707c8
SalvatorePaul <kehindepaul105@gmail.com>
2023-06-20 16:09:28 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,15 +2,15 @@
 #include <stdio.h>
 
 /**
- * int _abs - a function that computes the absolute value of an integer
- * @c: the integer to check
+ * _abs - a function that computes the absolute value of an integer
+ * @abs: the integer to check
  * Return: the anolute value of an integer
  */
 
 int _abs(int abs)
 {
-	 if ( abs >= 0)
+	if (abs >= 0)
 	return (abs);
-	 else
+	else
 	return (-abs);
 }
