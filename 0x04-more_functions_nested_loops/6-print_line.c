f6609001f8a1b59448af9e3ef7e457280eb45a7a
SalvatorePaul <kehindepaul105@gmail.com>
2023-06-22 12:32:21 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,26 +1,17 @@
 #include "main.h"
-
 /**
- * print_line - draws a straight line
- * @n: is the number of times the character _ should be printed
- * The line should end with a new line
- * Return: None
+ * print_line - draws a straight line of (n) characters long.
+ * @n: the number of underscores to print.
  */
 void print_line(int n)
 {
-	int i = 0;
+	int line;
 
-	if (n <= 0)
-	{
-	_putchar('\n');
-	}
-	else
-	{
-	while (i < n)
-	{
+	if (n > 0)
+
+	for (line = 0; line < n; line++)
+
 	_putchar('_');
-	i++;
-	}
+
 	_putchar('\n');
-}
 }
