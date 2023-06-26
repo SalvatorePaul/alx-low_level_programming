c6bd5910181467563bd5108ed476a2d2f291534e
SalvatorePaul <kehindepaul105@gmail.com>
2023-06-26 10:24:54 -0700

    101-keygen.c

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define PASSWORD_LENGTH 10
+
+char generateRandomChar() {
+    // Generate a random character between '!' and '~'
+    return rand() % ('~' - '!' + 1) + '!';
+}
+
+void generateRandomPassword(char *password) {
+    int i;
+    for (i = 0; i < PASSWORD_LENGTH - 1; i++) {
+        password[i] = generateRandomChar();
+    }
+    password[PASSWORD_LENGTH - 1] = '\0';
+}
+
+int main() {
+    // Seed the random number generator with the current time
+    srand(time(NULL));
+
+    // Generate a random password
+    char password[PASSWORD_LENGTH];
+    generateRandomPassword(password);
+
+    // Print the generated password
+    printf("Generated Password: %s\n", password);
+
+    return 0;
+}
