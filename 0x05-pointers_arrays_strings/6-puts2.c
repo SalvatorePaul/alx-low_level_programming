d7a7ee0b7c0a1740581a4955f9c41c8de292abc3
SalvatorePaul <kehindepaul105@gmail.com>
2023-06-26 08:53:41 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,19 +1,19 @@
 #include "main.h"
 /**
- * puts2 - This function prints every character of an even string
- * @str: The string parameter input
- * Return: Noting
+ * puts2 - This function prints every character of a string
+ * @str: The string to be printed
+ * Return: None
  */
 
 void puts2(char *str)
 {
-	int i = 0;
+	int prt;
 
-	while (str[i] != '\0')
+	for (prt = 0; str[prt] != '\0'; prt++)
 	{
-	_putchar(str[i]);
-	i += 2;
-	}
+		if (prt % 2 == 0)
+		_putchar(str[prt]);
 
+	}
 	_putchar('\n');
 }
