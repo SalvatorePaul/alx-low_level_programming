07d53cf9c059c8828f2cdb3bffc8c6951aa9f210
SalvatorePaul <kehindepaul105@gmail.com>
2023-06-29 11:27:33 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,22 @@
  * Return: a pointer to the resulting string dest
  */
 
-char* _strcat(char* dest, char* src)
+char *_strcat(char *dest, char *src)
 {
 	int index, indexSrc;
 
+	/*Find the size of dest array*/
 	for (index = 0; dest[index]; index++)
 		;
-	for (indexSrc = 0; src(indexSrc); indexSrc++, index++)
+
+	/* Append each src character to dest */
+	for (indexSrc = 0; src[indexSrc]; indexSrc++, index++)
 	{
-		dest(index) = src(indexSrc);
+		dest[index] = src[indexSrc];
 	}
-	dest(index) = '\0';
+
+	/* Add null terminator to the end of dest */
+	dest[index] = '\0';
+
 	return (dest);
 }
