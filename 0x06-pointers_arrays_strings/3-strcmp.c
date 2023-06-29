fab480d5648a37be0ad488562c5b6a1ea6a37ff3
SalvatorePaul <kehindepaul105@gmail.com>
2023-06-29 12:41:21 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,19 @@
  * if str1 > str2, the positive difference of the first unmatched characters
  */
 
-int _strcmp(char* s1, char* s2)
+int _strcmp(char *s1, char *s2)
 {
-while (*s1 && *s2 && s1 == *s2)
-{
-s1++;
-s2++;
-}
-return (*s1 - *s2);
+	int equal = 0;
+
+	while (*s1)
+	{
+		if (*s1 != *s2)
+		{
+			equal = ((int)*s1 - 48) - ((int)*s2 - 48);
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return (equal);
 }
