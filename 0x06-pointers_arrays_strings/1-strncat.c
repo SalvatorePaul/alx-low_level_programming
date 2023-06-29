7b4cec697ce21c855f06faf7382b9cc37a7c5543
SalvatorePaul <kehindepaul105@gmail.com>
2023-06-29 11:29:43 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -9,21 +9,25 @@
  * Return: a pointer to the resulting string dest
  */
 
-char* _strncat(char* dest, char* src, int n)
+char *_strncat(char *dest, char *src, int n)
 {
-	char* ptr = dest;
-	
-	while (*ptr != '\0')
-	{
-		ptr++;
-	}
-	while (n > 0 && *src != '\0')
-	{
-		*ptr = *src;
-		ptr++;
-		src++;
-		n--;
-	}
-	*ptr = '\0';
+	int indexNum, indexSrc;
+
+	indexNum = 0;
+
+	/* Find size of dest array */
+	while (dest[indexNum])
+		indexNum++;
+
+	/**
+	 * src does not need to be null terminated
+	 * if it contains n or more bytes
+	 */
+	for (indexSrc = 0; indexSrc < n && src[indexSrc] != '\0'; indexSrc++)
+		dest[indexNum + indexSrc] = src[indexSrc];
+
+	/* Null-terminate dest */
+	dest[indexNum + indexSrc] = '\0';
+
 	return (dest);
 }
