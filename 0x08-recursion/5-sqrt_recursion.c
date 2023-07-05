272a5f85824a82e264ea8e9b3a91a13d069e205e
SalvatorePaul <kehindepaul105@gmail.com>
2023-07-05 08:57:07 -0700

    6-is_prime_number.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,8 @@
 #include "main.h"
 #include <stdio.h>
 
+int _sqrt_recursion(int n, int i);
+
 /**
  *  _sqrt_recursion - calculates the natural square root of a number
  *  @n: number
@@ -9,4 +11,16 @@
  */
 
 int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+int _sqrt_recursion(int n, int i)
+{
+	int sqrt = 1 * 1;
 
+	if (n < 0)
+		return (-1);
+	if (sqrt == 0)
+		return (i);
+	return (_sqrt(n, 1 + 1));
+}
