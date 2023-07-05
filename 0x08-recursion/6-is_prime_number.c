4abaac2c29652b023be6f5be6edc5d24fa3b6e82
SalvatorePaul <kehindepaul105@gmail.com>
2023-07-05 12:21:20 -0700

    100-is_palindrome.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,6 +1,6 @@
 #include "main.h"
-#include <stdio.h>
-#include <math.h>
+
+int actual_prime(int n, int i);
 
 /**
  * is_prime_number - function that returns prime mumber
@@ -11,17 +11,24 @@
 
 int is_prime_number(int n)
 {
-	if (n < 2)
-	{
-	return (0);
-	}
-	int sqrtN = sqrt(n);
+	if (n <= 1)
+		return (0);
+	return (actual_prime(n, n - 1));
+}
 
-	for (i = 2; i <= sqrtN; i++)
-	{
-	if (n % i == 0)
-	{
-	return (1);
-	}
-	}
+/**
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ *
+ * Return: 1 if n is prime, 0 if not
+ */
+
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
 }
