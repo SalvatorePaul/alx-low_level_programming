eb8cd9d99b03cacb5f0e17930b084b5224510fc2
SalvatorePaul <kehindepaul105@gmail.com>
2023-07-27 12:25:07 -0700

    100

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,19 +1,10 @@
-#include <stdio.h>
+#include "lists.h"
 /**
  * printText - the text to be printed
  * Return: void
  */
 
-void printText()
+void printText(void)
 {
     printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-
-int main() {
-    // Call the function to print the text
-    printText();
-
-    // Rest of the main function code goes here
-
-    return 0;
 }
