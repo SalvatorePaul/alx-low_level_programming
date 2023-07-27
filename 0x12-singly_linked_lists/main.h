ed46f22bd9262273479e28372ace604bc30eb688
SalvatorePaul <kehindepaul105@gmail.com>
2023-07-27 06:02:18 -0700

    print_list

--- 0x12-singly_linked_lists/main.h
+++ 0x12-singly_linked_lists/main.h
@@ -1,6 +1,31 @@
 #ifndef SINGLY_LINKED_LIST_H
 #define SINGLY_LINKED_LIST_H
+#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
 
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for Holberton tasks
+ */
+/* Node structure */
+typedef struct list_s
+{
+	char *str;	/* Points to a dynamically allocated string */
+	unsigned int len;	/* Length of the string */
+	struct list_s *next;	/* Points to the next node in the list */
+} list_t;
+
+/* Function prototypes */
 size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
 #endif /*SINGLY_LINKED_LIST_H*/
