e57de908c42d13c310ebc8aafc314a9c18d807d2
SalvatorePaul <kehindepaul105@gmail.com>
2023-07-27 14:53:57 -0700

    101

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,21 @@
 section .data
-    hello db "Hello, Holberton", 0
-    format db "%s", 10, 0
+	hello db 'Hello, Holberton',0
+	format db '%s', 10, 0  ; The format specifier for the printf function
+	; "%s" specifies a string, and "10" is the ASCII value for newline (\n)
 
 section .text
-    extern printf
+	global main
 
-global _start
+extern printf   ; Declare the external function printf
 
-_start:
-    ; Call printf and exit the program
-    push hello
-    push format
-    call printf
-    add rsp, 16
+main:
+	; Call printf with the format string and the address of the hello string
+	mov rdi, format
+	mov rsi, hello
+	xor rax, rax  ; rax should be zero before the call
+	call printf
 
-    ; Exit the program
-    mov rax, 60         ; syscall number for exit
-    xor rdi, rdi        ; status = 0
-    syscall
+	; Exit the program
+	xor rdi, rdi  ; Return 0 as the exit code
+	mov rax, 60   ; syscall number for sys_exit
+	syscall
