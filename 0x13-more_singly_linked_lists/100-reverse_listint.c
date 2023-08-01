7380464ffa754f833f0893063fc90e52723d0862
SalvatorePaul <kehindepaul105@gmail.com>
2023-08-01 14:16:14 -0700

    reverse

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -11,14 +11,21 @@ listint_t *reverse_listint(listint_t **head)
 {
 	/* Declare variables to keep track of previous and current nodes */
 	listint_t *previous = NULL;
-	listint_t *current = NULL;
+	listint_t *currNode = NULL;
 
+	/* Loop through the linked list until the head pointer is NULL */
 	for (; *head != NULL; *head = currNode)
 	{
-		current = (*head)->next;
+		/* Store the next node in the list */
+		currNode = (*head)->next;
+		/* Reverse the link of the current node to point to the previous node */
 		(*head)->next = previous;
+		/* Update the previous node to the current node */
 		previous = *head;
 	}
+
+	/* After the loop, the head pointer will be pointing to the last node */
 	*head = previous;
+	/* Return the pointer to the first node of the reversed list */
 	return (*head);
 }
