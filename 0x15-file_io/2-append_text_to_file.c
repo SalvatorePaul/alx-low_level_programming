1f616a1c711959bf503b4e584449024e3a59b55b
SalvatorePaul <kehindepaul105@gmail.com>
2023-08-07 13:43:11 -0700

    append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,15 +12,9 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_APPEND | O_WRONLY), len = 0;
 
-	if (filename == NULL)
+	if (!filename || fd == -1)
 		return (-1);
-	if (text_content == NULL)
-		return (1);
 
-	FILE *file = fopen(filename, "a");
-
-	if (file == NULL)
-		return (-1);
 	while (text_content && text_content[len] != '\0')
 		len++;
 
