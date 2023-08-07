1edc275b8dc9b70720dcbf2e44aa6b16045366e7
SalvatorePaul <kehindepaul105@gmail.com>
2023-08-07 14:47:09 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,22 +8,22 @@
 *
 * Return: 0 on success
 */
-int main(int ac, char **av)
-{
-	int fd_from, fd_to, read_from, close_status, bytes_written, bytes_read;
-	char buf[1024];
 
-	if (ac != 3)
+int main(int argc, char **argv)
+{
+	int src_fd, dest_fd, close_status, bytes_written, bytes_read;
+	char buffer[1024];
+
+	if (argc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	fd_from = open(av[1], O_RDONLY);
-	if (fd_from == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]), exit(98);
-
-	fd_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-
-	if (fd_to == -1)
+	dest_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dest_fd == -1)
 		dprintf(STDERR_FILENO, WRITE_ERR, argv[2]), exit(99);
+
+	src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1)
+		dprintf(STDERR_FILENO, READ_ERR, argv[1]), exit(98);
 
 	while (1)
 	{
