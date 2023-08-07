dc25d4d8c9f43e53043f3b73070541c1e81e9672
SalvatorePaul <kehindepaul105@gmail.com>
2023-08-07 13:23:43 -0700

    create

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,15 +11,12 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, len = 0;
-	
-	if (filename == NULL)
-		return (-1);
-	
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 
+	if (filename == NULL)
+	return (-1);
+	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	
 	while (text_content && text_content[len] != '\0')
 		len++;
 
