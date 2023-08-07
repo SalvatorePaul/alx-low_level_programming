2ab9021c2ea347343eea7992d3663a636c1d6f63
SalvatorePaul <kehindepaul105@gmail.com>
2023-08-07 14:01:10 -0700

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -28,8 +28,14 @@ int main(int ac, char **av)
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 
+	if (read_from == -1)
+		dprintf(STDERR_FILENO, "Error; Can't read from %s\n", av[1]), exit(98);
+
 	if ((close(fd_from)) == -1)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from), exit(100);
+
+	if ((close(fd_to)) == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to), exit(100);
 
 	return (0);
 }
