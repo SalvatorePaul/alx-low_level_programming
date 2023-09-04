5293c7b23388336000160221f92334cf817e6668
SalvatorePaul <kehindepaul105@gmail.com>
2023-09-04 11:53:04 -0700

    io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,6 @@
  * Return: 1 on success, -1 of failure
  * -1 if filename is NULL
  */
-
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd = open(filename, O_APPEND | O_WRONLY), len = 0;
